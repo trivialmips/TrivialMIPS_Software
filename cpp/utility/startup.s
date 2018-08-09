6e007dffa9f6c370ab1494dbff6e116eacd0bc1d
Harry Chen <i@harrychen.xyz>
2018-08-09 17:17:31 +0800

    Add exception handling to cpp-coded software
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- cpp/utility/startup.s
+++ cpp/utility/startup.s
@@ -6,11 +6,24 @@
 	.globl _start
 	.type _start, @function
 _start:
+	# setup exception handler
+	lui		$t2, 0x0040
+	mtc0	$t2, $12 # c0_status
+	mtc0	$zero, $13 # c0_cause
 	# setup stack pointer
-	la $sp, 0x807ffff0
-	jal _Z6_entryv
+	la 		$sp, _stack
+	la		$gp, _gp
+	# jump to our code
+	jal 	_Z6_entryv
 	nop
 
 _loop0:
 	b _loop0
 	nop
+
+_exception:
+    .org 0x380
+    jal exception
+    nop
+    j _loop0
+    nop 
